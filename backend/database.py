e38551970593cfa615855bff4afc523d6b60cf18
SeungjunS0ng <john091122@naver.com>
2025-06-22 18:07:10 +0900

    프로젝트 설명을 위한 README 작성 및 아이디 찾기 폼에 전화번호 예시 placeholder 추가

---
+++ backend/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from backend.config.settings import settings 
+
+DATABASE_URL = f"mysql+mysqlconnector://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
